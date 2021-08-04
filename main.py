028514dc07eeb9105bb87b203d1fee7bfdb92245
karenm08 <wmok14@my.bcit.ca>
2021-08-04 03:09:00 -0700

    show random word when press btn

--- main.py
+++ main.py
@@ -4,21 +4,25 @@ import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+# read csv using panda
+data = pandas.read_csv("./data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+print(to_learn[0]["French"])
+
+
+def next_card():
+    current_card = random.choice(to_learn)  # eg : {'French': 'vers', 'English': 'towards'}
+    # random_english = current_card["English"]
+    random_french = current_card["French"]
+    canvas.itemconfig(card_title, text="French")  # change the text
+    canvas.itemconfig(card_word, text=random_french)  # change the text
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Flash Card")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-
-# read csv using panda
-data = pandas.read_csv("./data/french_words.csv")
-french_words_list = data["French"].to_list()
-english_words_list = data["English"].to_list()
-
-# pick random a word
-random_num = random.randint(0, len(french_words_list)-1)
-random_english = english_words_list[random_num]
-random_french = french_words_list[random_num]
 
 # same size as the card image
 canvas = Canvas(width=800, height=526)
@@ -36,16 +40,17 @@ canvas.grid(column=0, row=0, columnspan=2)
 canvas.config(bg=BACKGROUND_COLOR,  highlightthickness=0)
 
 # ---------------------------- Text on canvas --------------------------------------#
-canvas.create_text(400, 150, font="Ariel 40 italic", text="French")
-canvas.create_text(400, 263, font="Ariel 60 bold", text=random_french)
+card_title = canvas.create_text(400, 150, font="Ariel 40 italic", text="")
+card_word = canvas.create_text(400, 263, font="Ariel 60 bold", text="")
 
 # ---------------------------- Button --------------------------------------#
-right_button = Button(image=right_image, highlightthickness=0)
+right_button = Button(image=right_image, highlightthickness=0, command=next_card)
 right_button.grid(column=1, row=1)
 
-wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
 
-
+# show the word on the card after the UI / when start the app
+next_card()
 
 window.mainloop()
